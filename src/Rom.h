6fa55c236f96136dd10c3a77c7a92bf69fce86cc
PCYoshi <josephcaillet@gmail.com>
2015-11-15 22:15:01 +0100

    Rom class added

---
+++ src/Rom.h
@@ -0,0 +1,64 @@
+//
+// Created by joseph on 15/11/15.
+//
+
+#ifndef MAMEUISEN_GAME_H
+#define MAMEUISEN_GAME_H
+
+#include <string>
+#include <vector>
+
+class Rom
+{
+private:
+	std::string description;
+	std::string filename;
+	std::string manufacturer;
+	int year;
+	std::vector* categories<int> = nullptr;
+
+public:
+	Rom(const std::string& description, const std::string& filename, const std::string& manufacturer, int year)
+			: description(description), filename(filename), manufacturer(manufacturer), year(year)
+	{
+		categories = new std::vector<int>(0,0);
+	}
+
+	const std::string& getDescription() const
+	{
+		return description;
+	}
+
+	const std::string& getFilename() const
+	{
+		return filename;
+	}
+
+	const std::string& getManufacturer() const
+	{
+		return manufacturer;
+	}
+
+	int getYear() const
+	{
+		return year;
+	}
+
+	std::vector* getCategories() const
+	{
+		return categories;
+	}
+
+	void addCategory(int categoryNumber)
+	{
+		categories->push_back(categoryNumber);
+	}
+
+	void freeCategories()
+	{
+		delete categories;
+	}
+};
+
+
+#endif //MAMEUISEN_GAME_H

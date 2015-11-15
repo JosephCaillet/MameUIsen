3c8fefad3658f99dc42d8030d4547505dc8a0cdb
PCYoshi <josephcaillet@gmail.com>
2015-11-15 22:38:54 +0100

    RomSet class added

--- src/RomSet.h
+++ src/RomSet.h
@@ -5,10 +5,41 @@
 #ifndef MAMEUISEN_ROMSET_H
 #define MAMEUISEN_ROMSET_H
 
+#include <string>
+#include "Rom.h"
 
 class RomSet
 {
+private:
+	std::string name;
+	std::vector<Rom*> romList;
 
+public:
+	//Constructor
+	RomSet(const std::string& name) : name(name)
+	{ }
+
+	//Getters
+	const std::string& getName() const
+	{
+		return name;
+	}
+
+	const std::vector<Rom*>& getRomList() const
+	{
+		return romList;
+	}
+
+	//Member functions
+	bool isEmpty()
+	{
+		return romList.empty();
+	}
+
+	void addRom(Rom* rom)
+	{
+		romList.push_back(rom);
+	}
 };
 
 

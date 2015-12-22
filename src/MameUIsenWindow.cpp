1ea90d61a01779098f7aac3b4c739dc584bdb060
PCYoshi <josephcaillet@gmail.com>
2015-12-22 12:23:47 +0100

    algorithmic comments added

--- src/MameUIsenWindow.cpp
+++ src/MameUIsenWindow.cpp
@@ -10,3 +10,24 @@ MameUIsenWindow::MameUIsenWindow() : RenderWindow(), configuration(), romListMan
 	create(sf::VideoMode(configuration.getWindowWidth(), configuration.getWindowHeight()), "MameUIsen", sf::Style::Titlebar | sf::Style::Close	);
 	display();
 }
+
+void MameUIsenWindow::lauch()
+{
+	//Get the first category
+	//Display category name and quantity
+	//Display games titles
+	//Display image and info about the selected game
+	//If event change current game
+	//	update game list
+	//	Display category name and quantity
+	//	Display games titles
+	//	Display image and info about the selected game
+	//If event change category
+	//	update category
+	//	update game list
+	//	Display category name and quantity
+	//	Display games titles
+	//	Display image and info about the selected game
+	//If exit
+	//	quit
+}
--- src/MameUIsenWindow.h
+++ src/MameUIsenWindow.h
@@ -17,6 +17,7 @@ private:
 
 public:
 	MameUIsenWindow();
+	void lauch();
 };
 
 

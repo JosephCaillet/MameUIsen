8bb2bd9a789b28ee16271a5ebf02dd4682a213db
PCYoshi <josephcaillet@gmail.com>
2016-01-05 13:18:58 +0100

    Correct some typo mistakes

--- src/lib/EasyConfigurationFileReadingApi/AbstractConfiguration.h
+++ src/lib/EasyConfigurationFileReadingApi/AbstractConfiguration.h
@@ -2,8 +2,8 @@
 // Created by joseph on 30/12/15.
 //
 
-#ifndef MAMEUISEN_ABSTACTCONFIGURATION_H
-#define MAMEUISEN_ABSTACTCONFIGURATION_H
+#ifndef MAMEUISEN_ABSTRACTCONFIGURATION_H
+#define MAMEUISEN_ABSTRACTCONFIGURATION_H
 
 #include <vector>
 #include <fstream>
@@ -51,4 +51,4 @@ public:
 
 #include "AbstractConfiguration.tpp"
 
-#endif //MAMEUISEN_ABSTACTCONFIGURATION_H
+#endif //MAMEUISEN_ABSTRACTCONFIGURATION_H
